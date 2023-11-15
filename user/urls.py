c9e29ebdd4e79b752e8db5a5c64b17ebb8fbb04a
Стас Басов <stas.basov@whatever.com>
2023-11-15 23:18:06 +0300

    Login, logout, sign_in

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from user.views import user_sign_up
+from user.views import user_sign_up, user_login, user_logout
 
 app_name = 'user'
 
 urlpatterns = [
     path('', user_sign_up, name='sign_up'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from user.forms import UserSignUpForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.conf import settings
+from django.contrib import messages
+
+
+User = settings.AUTH_USER_MODEL
 
 
 def user_sign_up(request):
@@ -24,3 +29,35 @@ def user_sign_up(request):
     }
 
     return render(request, 'user_sign_up/sign_up.html', context)
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('main:index')
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except Exception:
+            messages.warning(request,
+                             f'Пользователя с почтой {email}, не существует')
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Все окей, проходи')
+            return redirect('main:index')
+        else:
+            messages.warning(request, 'Пользователя не существует')
+    context = {
+
+    }
+    return render(request, 'user_sign_up/login.html', context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('user:login')
